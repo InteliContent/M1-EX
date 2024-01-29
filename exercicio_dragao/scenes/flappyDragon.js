2d04288ffbcff70daea8658bb2b21bb8f196e8f2
brunamayer <bruna.mayer@inteli.edu.br>
2024-01-29 06:24:06 -0300

    exercicios alien e dragão

--- exercicio_dragao/scenes/flappyDragon.js
+++ exercicio_dragao/scenes/flappyDragon.js
@@ -1,22 +1,23 @@
+// Definindo a cena principal do jogo usando a biblioteca Phaser
 class FlappyDragon extends Phaser.Scene {
 
-
-    constructor(){
+    // Construtor da cena
+    constructor() {
         super({
             key: 'FlappyDragon',
-            // Se quisermos alterar alguma configuração do jogo para essa cena:
-            /* physics:{
-               arcade:{
+            // Configurações específicas da cena podem ser adicionadas aqui
+            /* physics: {
+               arcade: {
                 debug: false,
-                gravity: { y: 500}
+                gravity: { y: 500 }
                } 
             } */
         });
     }
 
-
-    init(){
-        // *** 1) FUNDO *****
+    // Inicialização de variáveis e configurações da cena
+    init() {
+        // 1) FUNDO
         // A imagem do fundo possui 1200 de largura e 400 de altura.
         // Mas somente uma janela de 400x400 é exibida por vez.
         // Essa janela vai se alterando, simulando o movimento do fundo
@@ -29,8 +30,9 @@ class FlappyDragon extends Phaser.Scene {
             obj: null
         };
 
-        // *** 2) COLUNA *****
+        // 2) COLUNA
         //Os parâmetros speed e space podem ser alterados para deixar o jogo mais fácil ou mais difícil
+
         this.cols = {
             speed: 60, // velocidade com que as colunas passam, quanto maior, mais rápidamente passam
             space: 180, // vão entre as colunas por onde o dragão tem que passar, quanto maior mais espaço entre as colunas
@@ -46,114 +48,110 @@ class FlappyDragon extends Phaser.Scene {
             col2_obj: null
         };
 
-        // *** 3) JOGADOR *****
+        // 3) JOGADOR
         this.player = {
             width: 170,
             height: 133,
             obj: null
         };
 
-        // *** 4) CONTROLES DA RODADA *****
+        // 4) CONTROLES DA RODADA
         this.gameControls = {
             over: false,
             current_col_scored: false,
             score: 0,
             restartBt: null
-        }; 
+        };
     }
 
-
-    preload ()
-    {
+    // Pré-carregamento de recursos
+    preload() {
         this.load.image('bg', 'img/fundo.png');
         this.load.spritesheet('dragon', 'img/dragao.png', { frameWidth: this.player.width, frameHeight: this.player.height });
         this.load.image('colBottom', 'img/coluna_bottom.png');
         this.load.image('colUpper', 'img/coluna_upper.png');
         this.load.image('gameOver', 'img/gameover.png');
-        this.load.image('restart', 'img/restart_bt.png')
+        this.load.image('restart', 'img/restart_bt.png');
     }
 
+    // Criação de elementos e configurações iniciais da cena
+    create() {
+        // 1) Adiciona a imagem de fundo
+        this.bg.obj = this.add.image(this.bg.x, this.bg.y, 'bg').setOrigin(0, 0);
 
-    create ()
-    {
-        /*Adiciona a imagem de fundo e a salva na chave "obj" da variável "bg"*/
-        this.bg.obj = this.add.image(this.bg.x, this.bg.y, 'bg').setOrigin(0,0);
-
-
-        /*Adiciona imagens das colunas*/
-        this.cols.col1_obj = this.add.image(this.cols.x, this.cols.y, 'colUpper').setOrigin(0,0);
-        this.cols.col2_obj = this.add.image(this.cols.x, this.cols.y + this.cols.height + this.cols.space, 'colBottom').setOrigin(0,0);
+        // 2) Adiciona imagens das colunas
+        this.cols.col1_obj = this.add.image(this.cols.x, this.cols.y, 'colUpper').setOrigin(0, 0);
+        this.cols.col2_obj = this.add.image(this.cols.x, this.cols.y + this.cols.height + this.cols.space, 'colBottom').setOrigin(0, 0);
         this.physics.add.existing(this.cols.col1_obj);
         this.physics.add.existing(this.cols.col2_obj);
-        this.cols.col1_obj.body.allowGravity = false; 
+        this.cols.col1_obj.body.allowGravity = false;
         this.cols.col2_obj.body.allowGravity = false;
-        this.cols.col1_obj.body.setVelocityX(-this.cols.speed); 
-        this.cols.col2_obj.body.setVelocityX(-this.cols.speed); 
+        this.cols.col1_obj.body.setVelocityX(-this.cols.speed);
+        this.cols.col2_obj.body.setVelocityX(-this.cols.speed);
 
-        /*Adiciona jogador e suas propriedades físicas*/
+        // 3) Adiciona jogador e suas propriedades físicas
         this.player.obj = this.physics.add.sprite(170, 130, 'dragon').setScale(.8);
-        this.player.obj.body.setSize(50,80,true);
+        this.player.obj.body.setSize(50, 80, true);
         this.player.obj.setCollideWorldBounds(true);
 
-
-        /*Adiciona animação da imagem do jogador*/
+        // 4) Adiciona animação da imagem do jogador
         this.anims.create({
             key: 'fly',
             frames: this.anims.generateFrameNumbers('dragon', { start: 0, end: 2 }),
             frameRate: 10,
             repeat: -1
         });
-        /*Adiciona a animação do movimento do jogador*/
+
+        // 5) Adiciona a animação do movimento do jogador
         this.player.obj.anims.play('fly');
 
-        /*Adiciona os cursores que movimentarão o jogador*/
+        // 6) Adiciona os cursores que movimentarão o jogador
         this.cursors = this.input.keyboard.createCursorKeys();
         this.pointer = this.input.activePointer;
 
-        /*Adiciona os monitores de colisão*/
+        // 7) Adiciona os monitores de colisão
         this.physics.add.overlap(this.player.obj, this.cols.col1_obj, this.hitCol, null, this);
         this.physics.add.overlap(this.player.obj, this.cols.col2_obj, this.hitCol, null, this);
 
-        /*Mostrar o placar*/
+        // 8) Mostra o placar
         this.scoreText = this.add.text(15, 15, this.game.name + ': 0', { fontSize: '20px', fill: '#000' });
-        this.highScoreText = this.add.text(0, 15, 'high score: ' + this.game.highScore, { fontSize: '20px', fill: '#000', align: 'right'});
-        this.highScoreText.x = this.game.config.width - this.highScoreText.width - 15
-        this.gameControls.restartBt = this.add.image(this.game.config.width/2-50, this.game.config.height/4*3, 
-                                                    'restart').setScale(.2).setOrigin(0,0).setInteractive().setVisible(false);
+        this.highScoreText = this.add.text(0, 15, 'high score: ' + this.game.highScore, { fontSize: '20px', fill: '#000', align: 'right' });
+        this.highScoreText.x = this.game.config.width - this.highScoreText.width - 15;
+        this.gameControls.restartBt = this.add.image(this.game.config.width / 2 - 50, this.game.config.height / 4 * 3,
+            'restart').setScale(.2).setOrigin(0, 0).setInteractive().setVisible(false);
 
-        this.gameControls.restartBt.on('pointerdown', function(){
-            /*Controla se o jogo acabou e se a tecla que o botão de reiniciar foi acionado*/
-            if(this.gameControls.over){
+        // 9) Adiciona evento de clique no botão de "reiniciar"
+        this.gameControls.restartBt.on('pointerdown', function () {
+            // Controla se o jogo acabou e se a tecla que o botão de reiniciar foi acionado
+            if (this.gameControls.over) {
                 this.gameControls.over = false;
                 this.gameControls.score = 0;
-                this.cols.x = - this.cols.width -1;
+                this.cols.x = -this.cols.width - 1;
                 this.scene.restart();
             }
-        },this)
+        }, this);
     }
 
+    // Atualização lógica do jogo a cada frame
+    update() {
+        // Controla se o jogo acabou e paraliza a cena (interrompendo a execução de "update")
+        if (this.gameControls.over) {
+            return;
+        }
 
-    update ()
-    {        
-        /*Controla se o jogo acabou e paraliza a cena (interrompendo a execução de "update")*/
-        if(this.gameControls.over){
-            return
-        }  
-
-        /*Atualiza a posicao da imagem de fundo*/
+        // Atualiza a posição da imagem de fundo
         this.bg.x--;
-        if (this.bg.x < this.bg.x_end){
+        if (this.bg.x < this.bg.x_end) {
             this.bg.x = this.bg.x_start;
         }
         this.bg.obj.x = this.bg.x;
 
-
-        /*Atualiza posicao das colunas*/
-        this.cols.x = this.cols.col1_obj.x
-        if (this.cols.x < - this.cols.width){
+        // Atualiza posição das colunas
+        this.cols.x = this.cols.col1_obj.x;
+        if (this.cols.x < -this.cols.width) {
             this.cols.x = Phaser.Math.FloatBetween(this.cols.min_x, this.cols.max_x); // sorteia o intervalo antes das próximas colunas
-            this.cols.col1_obj.x = this.cols.x
-            this.cols.col2_obj.x = this.cols.x
+            this.cols.col1_obj.x = this.cols.x;
+            this.cols.col2_obj.x = this.cols.x;
 
             this.cols.y = Phaser.Math.FloatBetween(this.cols.min_y, this.cols.max_y); // sorteia a posição vertical
             this.cols.col1_obj.y = this.cols.y;
@@ -162,39 +160,37 @@ class FlappyDragon extends Phaser.Scene {
             this.gameControls.current_col_scored = false;
         }
 
-        /*Inclui controle de movimentação do dragao*/
+        // Inclui controle de movimentação do dragão
         if (this.cursors.left.isDown)
-            this.player.obj.setX(this.player.obj.x-5);
+            this.player.obj.setX(this.player.obj.x - 5);
         else if (this.cursors.right.isDown)
-            this.player.obj.setX(this.player.obj.x+5);
+            this.player.obj.setX(this.player.obj.x + 5);
         else if (this.cursors.up.isDown || this.cursors.space.isDown || this.pointer.isDown)
-            this.player.obj.setY(this.player.obj.y-this.game.config.physics.arcade.gravity.y);
+            this.player.obj.setY(this.player.obj.y - this.game.config.physics.arcade.gravity.y);
         else if (this.cursors.down.isDown)
-            this.player.obj.setY(this.player.obj.y+this.game.config.physics.arcade.gravity.y);
+            this.player.obj.setY(this.player.obj.y + this.game.config.physics.arcade.gravity.y);
 
-
-        /*Verifica se o jogador passou pelas colunas*/
-        if (!this.gameControls.current_col_scored){
-            if (this.player.obj.x - this.player.width/2 > this.cols.x + this.cols.width){
+        // Verifica se o jogador passou pelas colunas
+        if (!this.gameControls.current_col_scored) {
+            if (this.player.obj.x - this.player.width / 2 > this.cols.x + this.cols.width) {
                 this.gameControls.score++;
                 this.gameControls.current_col_scored = true;
-                this.scoreText.setText( this.game.name + ': ' + this.gameControls.score);
+                this.scoreText.setText(this.game.name + ': ' + this.gameControls.score);
             }
         }
     }
 
-
-    hitCol(player_obj, col_obj){
+    // Função chamada quando o jogador colide com uma coluna
+    hitCol(player_obj, col_obj) {
         this.physics.pause();
         this.player.obj.anims.stop('fly');
         this.player.obj.setTint(0xff0000);
         this.gameControls.over = true;
-        this.add.image(this.game.config.width/2, this.game.config.height/2, 'gameOver').setScale(.5);
+        this.add.image(this.game.config.width / 2, this.game.config.height / 2, 'gameOver').setScale(.5);
         this.gameControls.restartBt.visible = true;
-        if (this.gameControls.score > this.game.highScore){
+        if (this.gameControls.score > this.game.highScore) {
             this.game.highScore = this.gameControls.score;
-            this.highScoreText.setText('high score: ' +this.game.highScore);
+            this.highScoreText.setText('high score: ' + this.game.highScore);
         }
     }
-
 }
--- exercicio_dragao/scenes/welcome.js
+++ exercicio_dragao/scenes/welcome.js
@@ -1,32 +1,32 @@
+// Definindo a cena de boas-vindas usando a biblioteca Phaser
 class Welcome extends Phaser.Scene {
 
-
-    constructor(){
+    // Construtor da cena
+    constructor() {
         super({
             key: 'Welcome',
-            //Alteração da configuracao do jogo para essa cena para acessarmos os elementos html da pagina
-            backgroundColor: '#000',
+            backgroundColor: '#000', // Configuração da cor de fundo da cena
         });
     }
 
-
-    preload(){
-        this.load.html("form", "form/form.html");
-        this.load.image("play", "img/play_bt.png")
+    // Pré-carregamento de recursos
+    preload() {
+        this.load.html("form", "form/form.html"); // Carregando o formulário HTML
+        this.load.image("play", "img/play_bt.png"); // Carregando a imagem do botão "play"
     }
 
-
-    create(){
-
+    // Função chamada quando a cena é criada
+    create() {
+        // Configuração das teclas e variáveis da cena
         this.cursors = this.input.keyboard.createCursorKeys();
         this.returnKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
-
         this.nameFilled = false;
-        var text = {height: 20, padding: 15, content: "Hello --"}
 
+        // Configuração do texto de boas-vindas
+        var text = { height: 20, padding: 15, content: "Hello --" }
         this.message = this.add.text(
-            this.game.config.width/2, 
-            this.game.config.height/2 - text.padding*2 - text.height,
+            this.game.config.width / 2,
+            this.game.config.height / 2 - text.padding * 2 - text.height,
             text.content, {
                 color: "#FFFFFF",
                 fontSize: 40,
@@ -34,58 +34,56 @@ class Welcome extends Phaser.Scene {
             }
         ).setOrigin(0.5);
 
-        
-        //var html = '<input type="text" name="name" placeholder="Your name" />';
-        //this.nameInput = this.add.dom(300, 200).createFromHTML(html).setOrigin(0,0);
-        var inputSize = {width: 270, height: 42, padding: 15}; //input size including margins and paddings    
-        var inputButton = {width: 30, height: 12};
+        // Configuração do formulário de entrada de nome
+        var inputSize = { width: 270, height: 42, padding: 15 };
+        var inputButton = { width: 30, height: 12 };
         var inputCoords = {
-            xposition: (this.game.config.width-inputSize.width)/2 - inputButton.width,
-            yposition: (this.game.config.height-inputSize.height-inputSize.padding*2)/2,
-        };  
+            xposition: (this.game.config.width - inputSize.width) / 2 - inputButton.width,
+            yposition: (this.game.config.height - inputSize.height - inputSize.padding * 2) / 2,
+        };
+        this.inputName = this.add.dom(inputCoords.xposition, inputCoords.yposition).createFromCache('form').setOrigin(0, 0);
 
-       this.inputName = this.add.dom(inputCoords.xposition, inputCoords.yposition).createFromCache('form').setOrigin(0,0);
-        
-       const nameOkTextButton = this.add.text(
-            inputCoords.xposition + inputSize.width + 13, 
-            //padding desse elemento + aproximadamente 3 pixels do caracter ">" utilizado no tamanho de fonte escolhido
-            inputCoords.yposition + inputButton.height + 2, ">", 
-            {backgroundColor: "#8ecbf4", fontSize: 18, padding: 10}
-        )
+        // Configuração do botão para confirmar o nome
+        const nameOkTextButton = this.add.text(
+            inputCoords.xposition + inputSize.width + 13,
+            inputCoords.yposition + inputButton.height + 2, ">", {
+                backgroundColor: "#8ecbf4",
+                fontSize: 18,
+                padding: 10
+            }
+        );
         nameOkTextButton.setInteractive();
-        
+
+        // Configuração de eventos para atualizar o nome
         this.returnKey.on("down", event => {
             this.updateName(this.inputName);
         });
 
         nameOkTextButton.on('pointerdown', () => {
             this.updateName(this.inputName);
-        })
+        });
 
-        this.playBt = this.add.image(this.game.config.width/2-50, this.game.config.height/4*3,'play')
-                              .setScale(.2).setOrigin(0,0).setInteractive().setVisible(false);
-        
-        this.playBt.on('pointerdown', function() {
-            if(this.nameFilled){
-                this.game.highScore = 0
+        // Configuração do botão de "play"
+        this.playBt = this.add.image(this.game.config.width / 2 - 50, this.game.config.height / 4 * 3, 'play')
+            .setScale(.2).setOrigin(0, 0).setInteractive().setVisible(false);
+
+        // Configuração de evento para iniciar o jogo ao clicar no botão "play"
+        this.playBt.on('pointerdown', function () {
+            if (this.nameFilled) {
+                this.game.highScore = 0;
                 this.scene.start('FlappyDragon', this.game);
             }
         }, this);
     }
 
-
-    update(){
-        return
-    }
-
-
-    updateName(inputNameElement){
+    // Função para atualizar o nome quando o usuário pressiona Enter ou clica no botão de confirmação
+    updateName(inputNameElement) {
         let name = inputNameElement.getChildByName("name");
-            if(name.value != "") {
-                this.message.setText("Hello " + name.value);
-                this.playBt.setVisible(true);
-                this.nameFilled = true;
-                this.game.name = name.value;
-            }
+        if (name.value != "") {
+            this.message.setText("Hello " + name.value);
+            this.playBt.setVisible(true);
+            this.nameFilled = true;
+            this.game.name = name.value;
+        }
     }
 }
